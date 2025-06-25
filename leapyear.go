15d8dacba677a3341eb224037cd1a4e74896e038
team9 <team9@example.com>
2025-06-25 02:54:08 +0000

    NO BUG

--- leapyear.go
+++ leapyear.go
@@ -1,5 +1,15 @@
 package main
 
 func leapyear(y int) bool {
-    return true
+	if (y%4==0){
+		if(y%100==0&&y%400!=0){
+			return false
+		}
+		return true
+	}else{
+	return false
+  }
+}
+func main(){
+
 }
--- leapyear_test.go
+++ leapyear_test.go
@@ -5,7 +5,28 @@ import (
 )
 
 func TestLeapYear01(t *testing.T) {
-    expected := leapyaer(1)
+    expected := leapyear(2004)
+    actual := true
+    if expected != actual {
+        t.Errorf("Error")
+    }
+}
+func TestLeapYear02(t *testing.T) {
+    expected := leapyear(2100)
+    actual := false
+    if expected != actual {
+        t.Errorf("Error")
+    }
+}
+func TestLeapYear03(t *testing.T) {
+    expected := leapyear(2001)
+    actual := false
+    if expected != actual {
+        t.Errorf("Error")
+    }
+}
+func TestLeapYear04(t *testing.T) {
+    expected := leapyear(2000)
     actual := true
     if expected != actual {
         t.Errorf("Error")
